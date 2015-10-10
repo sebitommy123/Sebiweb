a76bb8cf4c9dac604b692d147074168b94df022a
OtreblaLOL <albertobermejo.nunez@gm,ail.com>
2015-10-10 13:39:45 +0200

    Projecto2
    asdfghjl

---
+++ index.php
@@ -0,0 +1,2 @@
+<a href="proyecto1/index.php">chistes</a>
+<a href="projecto2/index.php">juegos</a>
---
+++ projecto2/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<link rel='stylesheet' href='estilo.css'>
+
+</head>
+
+<body>
+
+<form>
+
+
+<input type='password' placeholder = 'password' size = '35' class='boxfont'></input>	
+<br>
+	
+<input type='password' placeholder = 'password' size = '35' class='boxfont'></input>
+<br>
+
+<input type='button' value='submit'></input >
+	
+	
+	
+
+
+</form>
+
+</body>
+
+</html>
